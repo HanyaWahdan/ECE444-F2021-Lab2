e178f060f1f7e525d6a0402bc2d4b07b752c869f
HanyaWahdan <HanyaWahdan@github.com>
2021-09-26 21:09:28 -0400

    Activity 3 modification

--- hello.py
+++ hello.py
@@ -1,13 +1,30 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
-@app.route('/')
-def index():
-   return '<h1>Hello World!</h1>'
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app) 
 
 @app.route('/user/<name>')
-def user(name):
-   return '<h1>Hello, %s!</h1>' % name
+def index(name):
+   #hardcoded method
+   #return '<h1>Hello World!</h1>'
+
+   #Now adding datetime variable
+   name=name
+   return render_template('user.html',current_time=datetime.utcnow(),name=name)
+   
+
+#@app.route('/user/<name>')
+#def user(name):
+  # hardcoded method
+  # return '<h1>Hello, %s!</h1>' % name
+
+  # Now adding Bootstrap
+ # return render_template('user.html',name=name)
+
 
 if __name__ == '__main__':
    app.run(debug=True)
